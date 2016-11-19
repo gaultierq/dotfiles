2645c0d0e298f319966e4c2cdfdb5fd3a7e06750
Quentin Gaultier <quentin.gaultier@gmail.com>
2016-11-20 07:50:41 +0800

    config ++

---
+++ AndroidStudio2.2/scratches/scratch.java
@@ -0,0 +1,7 @@
+
+    private static class ParcelConverter extends com.babysittor.ModelDataParcelConverter {
+
+        @Override
+        public DataDefinition getDefinition() {
+            return UserData.DEFINITION;
+        }

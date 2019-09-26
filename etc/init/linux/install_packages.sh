b363d09ddf15854c36edd584251e309d7aa1ff98
Quentin Gaultier <quentin.gaultier@gmail.com>
2019-09-26 12:31:26 -0100

    fix stuff

--- etc/init/linux/install_packages.sh
+++ etc/init/linux/install_packages.sh
@@ -14,16 +14,24 @@ openvpn \
 unzip \
 wget && 
 
-
+is_exists() {
+    which "$1" >/dev/null 2>&1
+    return $?
+}
 
 echo "Installing zsh"
 sudo apt-get -y --force-yes install zsh
 sudo chsh -s "$(command -v zsh)" "${USER}"
 
-echo "Installing vim (python compiled)"
-mkdir tmp && cd /tmp && git clone https://github.com/vim/vim.git && cd vim
-./configure --enable-pythoninterp --prefix=/usr
-make && sudo make install
+
+if is_exists "vim"; then
+	:
+else
+	echo "Installing vim from source"
+	mkdir tmp && cd /tmp && git clone https://github.com/vim/vim.git && cd vim
+	./configure --enable-pythoninterp --prefix=/usr
+	make && sudo make install
+fi
 
 # cleaning unused dependencies
 sudo apt-get autoremove -y --force-yes

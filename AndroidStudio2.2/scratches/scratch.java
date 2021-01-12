14fe654ad32335acfaf9ee95cee489e2ea32d553
Quentin Gaultier <quentin.gaultier@gmail.com>
2021-01-12 09:40:25 +0100

    mv android studio prefs to git

--- AndroidStudio2.2/scratches/scratch.java
+++
@@ -1,7 +0,0 @@
-
-    private static class ParcelConverter extends com.babysittor.ModelDataParcelConverter {
-
-        @Override
-        public DataDefinition getDefinition() {
-            return UserData.DEFINITION;
-        }

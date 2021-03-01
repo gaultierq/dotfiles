8eb9da085c263bd7446f56c063293a3e255868be
Quentin Gaultier <quentin.gaultier@gmail.com>
2021-03-01 12:24:28 +0000

    dot ++

--- etc/init/linux/install_packages.sh
+++ etc/init/linux/install_packages.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -e
 
 
-echo "No packages to install here..."
+echo "Installing broot"
+echo "deb http://packages.azlux.fr/debian/ buster main" | sudo tee /etc/apt/sources.list.d/azlux.list
+wget -qO - https://azlux.fr/repo.gpg.key | sudo apt-key add -
+
+sudo apt-get update &&
+
+sudo apt-get -y --force-yes install broot
 

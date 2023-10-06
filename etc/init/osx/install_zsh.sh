90d046d783517aee51a9b6ede6ce9b849ace9dcb
Quentin Gaultier <quentin.gaultier@gmail.com>
2023-10-06 11:34:44 +0200

    dot ++

--- etc/init/osx/install_zsh.sh
+++ etc/init/osx/install_zsh.sh
@@ -6,9 +6,15 @@ echo "installing zsh..."
 
 HOMEBREW_NO_AUTO_UPDATE=1 brew install --cask iterm2
 HOMEBREW_NO_AUTO_UPDATE=1 brew install zsh
-# install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 zsh --version
+
+if [ ! -d "$ZSH" ] then
+    echo "installing oh-my-zsh"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+
 upgrade_oh_my_zsh
 
 echo "installing oh-my-zsh plugins..."

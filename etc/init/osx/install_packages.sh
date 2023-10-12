e6c760d07a899a8278e73ccdc40c152819699062
Quentin Gaultier <quentin.gaultier@gmail.com>
2023-10-12 09:52:29 +0200

    dots ++

--- etc/init/osx/install_packages.sh
+++ etc/init/osx/install_packages.sh
@@ -2,6 +2,22 @@
 
 set -e
 
-# install ssh keys from lastpass
-sudo gem install lastpass-ssh
-lastpass-ssh
+if [[ ! "$(type -P brew)" ]]; then
+
+	echo "Installing Homebrew"
+	sudo -v
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	sudo -k
+fi
+
+echo "brew update and upgrade..."
+brew update && brew upgrade
+
+cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+if ! brew bundle check; then
+	echo "Installing Homebrew missing packages"
+	brew bundle	
+fi
+cd -
+
+sh
--- etc/init/osx/install_ssh_keys.sh
+++ etc/init/osx/install_ssh_keys.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 
+
+
 set -e
 
-if [[ ! "$(type -P brew)" ]]; then
-
-	echo "Installing Homebrew"
-	sudo -v
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-	sudo -k
-fi
-
-echo "brew update and upgrade..."
-brew update && brew upgrade
-
-cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-if ! brew bundle check; then
-	echo "Installing Homebrew missing packages"
-	brew bundle	
-fi
-cd -
+# install ssh keys from lastpass
+sudo gem install lastpass-ssh
+lastpass-ss

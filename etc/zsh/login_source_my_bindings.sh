5149e354ba2c274feb02b28e60f6bb3c0363ad6b
Quentin Gaultier <quentin.gaultier@gmail.com>
2024-11-30 12:17:52 +0100

    more dots

---
+++ etc/zsh/login_source_my_bindings.sh
@@ -0,0 +1,19 @@
+
+lazygit-widget() {
+  lazygit
+}
+
+zle -N lazygit-widget
+bindkey '^Z' lazygit-widget
+
+
+#  -----
+function _autojump() {
+  cd $( sort -nr /Users/q/Library/autojump/autojump.txt | awk '{print $2}' | head -n 5 | tac | fzf +s --tac )
+  # git switch -q $( git-recently-checkedout-branches | tac | awk -F'\t' '{print $2}' | fzf +s --tac --preview='git log --oneline {} | head -20')
+  zle reset-prompt
+}
+
+# Create a ZSH "Widget" and bind to key (CTRL+G here)
+zle -N autojump-widget _autojump
+bindkey '^F' autojump-widget

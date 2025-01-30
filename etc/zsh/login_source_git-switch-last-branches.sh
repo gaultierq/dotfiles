6c3951652aca7ead12b82e32fa84a7ad825b00e1
Quentin Gaultier <quentin.gaultier@gmail.com>
2025-01-30 18:39:39 +0100

    dot ++

--- etc/zsh/login_source_git-switch-last-branches.sh
+++ etc/zsh/login_source_git-switch-last-branches.sh
@@ -1,13 +1,4 @@
-## Add this to your `~/.zshrc` file to have CTRL+G let you select your recent branches
-## Open a new terminal, or run `source ~/.zshrc` after adding
-## Requires: fzf (`brew install fzf`)
-#
-# Function to select recent branches
-function gsl() {
-  git switch -q $( git-recently-checkedout-branches | tac | awk -F'\t' '{print $2}' | fzf +s --tac --preview='git log --oneline {} | head -20')
-  zle reset-prompt
-}
+#!/bin/zsh
 
-# Create a ZSH "Widget" and bind to key (CTRL+G here)
-zle -N gsl-widget gsl
-bindkey '^G' gsl-widget
+zle -N kcs-widget kcs
+bindkey '^K' kcs-widget
